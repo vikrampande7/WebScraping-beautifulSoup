01c6c573a2a848672b6ce537f578a95f6e38cbe3
Vikram Pande <vikrampande783@gmail.com>
2022-05-19 21:26:30 +0530

    files_updated

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ print(all_anchor_tags)
 for tag in all_anchor_tags:
     print(tag.get_text())
     print(tag.get("href"))
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
---
+++ readme.md
@@ -0,0 +1,4 @@
+##Web Scraping using BeautifulSoup
+documentation - [click here] (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+
+- basic commands
